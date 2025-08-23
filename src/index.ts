d0149ee6f57dee8203e8a680a3dfee02cfe3cda6
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-08-23 09:46:47 +0300

    ADD connection to db live via render

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { type Request, type Response } from "express";
 import "dotenv/config"; // oneliner for configuration
 import { validateSecret } from "./security/validateEnv.js";
-import { closeDB, runDB } from "./db/database.js";
+import { closeDB, runDB, getDB } from "./db/database.js";
+import type { Db } from "mongodb";
 
 const app = express();
 const port: number = Number(process.env.PORT) || 3000;
@@ -12,7 +13,7 @@ interface User {
   name: string;
 }
 
-const user: User = {id: 1, name: "tobbe"};
+const user: User = { id: 1, name: "tobbe" };
 
 app.get("/user", (req, res) => {
   res.status(201).send(user);
@@ -21,6 +22,21 @@ app.get("/user", (req, res) => {
 app.get("/:id", (req: Request, res: Response) => {
   const id = req.params.id;
   res.send({ message: `id = ${id}` });
+});
+
+app.get("/api/v1/database/comments/:username", async (req, res) => {
+  const db: Db = getDB();
+  const result = await db
+    .collection("comments")
+    .find({ name: req.params.username }) // Mercedes Tyler
+    .limit(25)
+    .toArray();
+
+  if (result.length === 0) {
+    res.status(404).send({ message: "Nothing was found" });
+    return;
+  }
+  res.send(result);
 });
 
 async function startServer() {
